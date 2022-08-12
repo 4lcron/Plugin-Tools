6c3e1b3588fa326184f75a39fd6605cb4897f982
4lch4 <git@4lch4.io>
2022-08-12 04:12:09 -0500

    feat: much needed cleanup of root index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
 import { IJobInput, IJobOutput } from './interfaces/index.js'
-import { IOUtil } from './utils/index.js'
+import { DiscordUtil, IOUtil } from './lib/index.js'
 
-export * from './utils/index.js'
-export * from '@4lch4/logger'
-export { IJobInput, IJobOutput }
-
+// #region Helper/Utility Functions
 /**
- * Initialize a Cronicle job. Returns the job input object read from STDIN.
+ * Initialize a Cronicle job. Returns the job input object read from STDIN. The
+ * `Params` generic type is the type of object that is passed in as custom
+ * properties for the plugin.
  *
  * @returns The input object for the job.
  */
-export async function initJob<P>(): Promise<IJobInput<P>> {
+export async function initJob<Params>(): Promise<IJobInput<Params>> {
   const ioUtil = new IOUtil()
 
-  return ioUtil.readPipedInput<IJobInput<P>>()
+  return ioUtil.readPipedInput<IJobInput<Params>>()
 }
 
 /**
@@ -33,3 +32,28 @@ export async function endJob(output: IJobOutput): Promise<void> {
   console.log(JSON.stringify(output))
   process.exit(output.code)
 }
+// #endregion Helper/Utility Functions
+
+const dUtil = new DiscordUtil({
+  webhookUrl:
+    'https://discord.com/api/webhooks/1000947228678049913/141v4iiAHzMfxiSqzSVXDz2Z0nHRm2aT80kSEP0CQXU8MmmTzoGUSoturdO7iv3CZ_OJ'
+})
+
+dUtil
+  .sendMsg({
+    title: 'Hello, world.',
+    description: 'This is a test message.',
+    text: 'Ehhh... Is this thing on?'
+  })
+  .then(res => {
+    console.log(res)
+    console.log('Execution completed successfully!')
+  })
+  .catch(err => {
+    console.error('Error received during sendMsg() execution:')
+    console.error(err)
+  })
+
+export * from '@4lch4/logger'
+export * from './lib/index.js'
+export { IJobInput, IJobOutput }
